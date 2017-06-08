63838724c6f71f4d5138ff8f57ec5e178cc86d8f
Elvis Sun <elvissunlihe@gmail.com>
2017-06-08 14:34:58 -0400

    upload dataset

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get install python-setuptools python-dev build-essential 
 sudo easy_install pip 
+sudo apt-get install python-tk
 
 # get NVDIA support
 sudo apt-get install libcupti-dev
