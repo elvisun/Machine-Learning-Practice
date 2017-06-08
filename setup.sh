1e257fa562e2146b088c130d351588bf9bf7b9bf
Elvis Sun <elvissunlihe@gmail.com>
2017-06-08 14:21:44 -0400

    tensor flow

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ sudo apt-get update
 sudo apt-get install python-setuptools python-dev build-essential 
 sudo easy_install pip 
 
+# get NVDIA support
+sudo apt-get install libcupti-dev
+
 sudo pip install -r requirements.txt
